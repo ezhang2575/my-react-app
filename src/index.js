84d33075933f4e101e21159f7189888df8772c3c
ezhang2575 <676789710@qq.com>
2025-01-19 21:52:44 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import MonthlyMetrics from './MonthlyMetrics';
+import MaintenanceDualView from './MaintenanceDualView';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <MonthlyMetrics />
+    <div>
+      <MonthlyMetrics />
+      <MaintenanceDualView />
+    </div>
   </React.StrictMode>
 );
